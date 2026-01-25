a757944e50990bd482a24d047ab74a856ab342bc
stha9840 <aayushstha610@gmail.com>
2026-01-26 02:00:23 +0545

    Create Express server entry point with database initialization

---
+++ autovault-backend/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const app = require('./index')
+
+const PORT = process.env.PORT || 5000;
+
+
+// Start server
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
+
+
